c40475708cb009a4028ca89715245fd0b3782833
jlramos <jlramos@consisint.com>
2019-05-23 00:40:37 -0500

    TEST - Test solucion al problema plateado.

--- src/test/java/com/almundo/callCenter2019/AppTest.java
+++ src/test/java/com/almundo/callCenter2019/AppTest.java
@@ -1,38 +1,31 @@
 package com.almundo.callCenter2019;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.Assert.assertEquals;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.junit.Test;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+
+public class AppTest{
+	
+	@Test
+	public void process10Calls()  {
+		System.out.println( "******************** CALL CENTER ALMUNDO 2019 ********************" );
+        int callDemand = 10;
+        Dispatcher dispatcher = new Dispatcher(callDemand);
+        dispatcher.dispatchCall();
+        dispatcher.report();
+        assertEquals("No se pudieron atender las 10 llamadas solicitadas", 10, dispatcher.getProcessedCalls());
+	}
+	
+	@Test
+	public void process30Calls()  {
+		System.out.println( "******************** CALL CENTER ALMUNDO 2019 ********************" );
+        int callDemand = 30;
+        Dispatcher dispatcher = new Dispatcher(callDemand);
+        dispatcher.dispatchCall();
+        dispatcher.report();
+        int calls = dispatcher.getProcessedCalls() + dispatcher.getCallsToMake() + dispatcher.getHungCalls();
+        assertEquals("No se pudieron atender las 30 llamadas solicitadas", 30, calls);
+	}
 }
